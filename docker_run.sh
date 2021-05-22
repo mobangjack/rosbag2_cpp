b2bb3469eb4590fca370220ff3019894bbc46398
mobangjack <mobangjack@foxmail.com>
2021-05-22 23:42:41 +0800

    Fix docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,21 +2,25 @@
 
 cd $(dirname $0)
 
-cmd="cd ."
-append_cmd() {
-    cmd="$cmd && $1"
-}
+cmd="bash"
 
-append_cmd "groupadd -g $(id -g)"
-append_cmd "useradd -u $(id -u) -g $(id -g) -md -s /bin/bash"
-append_cmd "chown $(id -u):$(id -g) /home/$USER"
-append_cmd "cd $PWD"
-append_cmd "su - $USER"
+subcmd=""
+if [ $(id -u) != 0 ]; then
+    subcmd="groupadd -g $(id -g) $(id -ng)"
+    subcmd="$subcmd && useradd -u $(id -u) -g $(id -g) -md $HOME -s /bin/bash $USER"
+    subcmd="$subcmd && chown $(id -u):$(id -g) $HOME"
+    subcmd="$subcmd && cd $PWD"
+    subcmd="$subcmd && su - $USER"
+fi
 
 option="-it"
 if [ -n "$*" ]; then
-    append_cmd "bash -c \"$*\""
+    if [ -n "$subcmd" ]; then
+        cmd="$cmd -c \"$subcmd\" -c \"$*\""
+    else
+        cmd="$cmd -c \"$*\""
+    fi
     option="--rm"
 fi
 
-docker run -v $PWD:$PWD $option mobangjack/colcon-ndk:latest "$cmd"
+docker run -v $PWD:$PWD $option mobangjack/colcon-ndk:latest bash -c "$cmd"
